0d3f48665dd6de6b8d70cbe354caa7afda4d43e9
unknown <peiwen.lin@fmi10500674.minfound.com>
2017-03-31 16:24:28 +0800

    lost update shareDefine

--- testLeakConsoleExe/ShareDefine.h
+++ testLeakConsoleExe/ShareDefine.h
@@ -1,7 +1,7 @@
 #pragma once  
 
-#define MEMORY_SIZE 1024  
 #define GLOBAL_MEMORY_IN_NAME _T("ShareMemoryIN")  
 #define GLOBAL_MEMORY_OUT_NAME _T("ShareMemoryOUT") 
-#define GLOBAL_EVENT_IN_NAME _T("ShareMemoryEventIN")  
+#define GLOBAL_EVENT_IN1_NAME _T("ShareMemoryEventIN1")  
+#define GLOBAL_EVENT_IN2_NAME _T("ShareMemoryEventIN2")  
 #define GLOBAL_EVENT_OUT_NAME _T("ShareMemoryEventOUT") 
